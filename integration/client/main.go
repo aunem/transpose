17e197d1cc21fa53594fdf44df471a1abc2d65b6
grillz <patrickbarkerco@gmail.com>
2018-03-25 11:20:23 -0600

    http working!

--- integration/client/main.go
+++ integration/client/main.go
@@ -9,10 +9,17 @@ import (
 )
 
 func main() {
+	c := http.DefaultClient
 	for {
 		fmt.Println("making request...")
 		var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-		resp, err := http.Post("http://transpose", "application/json", bytes.NewBuffer(jsonStr))
+		r, err := http.NewRequest(http.MethodPost, "http://transpose:8080", bytes.NewBuffer(jsonStr))
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("request: %+v", r)
+		resp, err := c.Do(r)
+		// resp, err := http.Post("http://transpose:8080", "application/json", bytes.NewBuffer(jsonStr))
 		if err != nil {
 			log.Println("err: ", err)
 		}
